9ba0b93c5e925754347d6044acbe80d2bf53b449
Indwoo <wjsdlsdn1158@naver.com>
2023-12-02 16:03:37 +0900

    벡터 변환 작업

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pandas as pd
 import nltk
 from nltk.stem import PorterStemmer, WordNetLemmatizer
 from nltk.tokenize import word_tokenize
+import numpy as np
+from sklearn.preprocessing import LabelBinarizer
 
 nltk.download('punkt')
 nltk.download('wordnet')
@@ -32,3 +34,15 @@ book_norm['lemmatized_title'] = lemmatized_titles
 
 book_norm.to_csv('book_stemming_2.csv', index=False)
 
+# LabelBinarizer를 사용하여 User-ID를 벡터로 변환
+label_binarizer = LabelBinarizer()
+user_vector = label_binarizer.fit_transform(user_norm['User-ID'])
+
+# 자신의 User-ID에 해당하는 인덱스를 1로, 나머지 인덱스를 0으로 설정
+user_vector = np.where(user_vector == user_vector.max(axis=1, keepdims=True), 1, 0)
+
+# 변환된 벡터를 DataFrame에 추가
+user_vector_df = pd.DataFrame(user_vector, columns=label_binarizer.classes_)
+user_norm = pd.concat([user_norm, user_vector_df], axis=1)
+
+user_norm.to_csv('user_features.csv', index=False)
