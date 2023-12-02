3495843e42d5202b47400857193f4b768ff9927d
Indwoo <wjsdlsdn1158@naver.com>
2023-12-02 15:21:26 +0900

    스테밍 결과 확인용2

--- main.py
+++ main.py
@@ -15,12 +15,20 @@ rating_norm = rating.dropna()
 user_norm = user.dropna()
 
 # 어간 추출
-stemmer = PorterStemmer()
-book_norm['stemmed_title'] = book_norm['Title'].apply(lambda x: ' '.join([stemmer.stem(word) for word in word_tokenize(x)]))
+stemmed_titles = []
+stemmer = nltk.stem.PorterStemmer()
+for title in book_norm['Title']:
+    stemmed_title = ' '.join([stemmer.stem(word) for word in word_tokenize(title)])
+    stemmed_titles.append(stemmed_title)
+book_norm['stemmed_title'] = stemmed_titles
 
 # 표제어 추출
+lemmatized_titles = []
 lemmatizer = WordNetLemmatizer()
-book_norm['lemmatized_title'] = book_norm['Title'].apply(lambda x: ' '.join([lemmatizer.lemmatize(word) for word in word_tokenize(x)]))
+for title in book_norm['stemmed_title']:
+    lemmatized_title = ' '.join([lemmatizer.lemmatize(word) for word in word_tokenize(title)])
+    lemmatized_titles.append(lemmatized_title)
+book_norm['lemmatized_title'] = lemmatized_titles
 
-book_norm.to_csv('book_stemming.csv', index=False)
+book_norm.to_csv('book_stemming_2.csv', index=False)
 
