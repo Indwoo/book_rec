7013dc882489d2e0a3addfcdce1e4b71ee6c0079
Indwoo <wjsdlsdn1158@naver.com>
2023-12-02 13:53:02 +0900

    rating user 결측치 제거

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import csv
 import pandas as pd
 
-data = pd.read_csv('archive/Ratings.csv', delimiter=';')
-data = data.dropna()
-print(data)
+book = pd.read_csv('archive/Books.csv', delimiter=';')
+rating = pd.read_csv('archive/Ratings.csv', delimiter=';')
+user = pd.read_csv('archive/Users.csv', delimiter=';')
+
+book_norm = book.dropna()
+rating_norm = rating.dropna()
+user_norm = user.dropna()
+
+print(book_norm)
+print(rating_norm)
+print(user_norm)
