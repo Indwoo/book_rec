21fe7f1ea1372ebeb76c67ad5371c1ad343e34b9
Indwoo <wjsdlsdn1158@naver.com>
2023-12-02 16:22:47 +0900

    user csv 정규화

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ user = pd.read_csv('archive/Users.csv', delimiter=';')
 book_norm = book.dropna()
 rating_norm = rating.dropna()
 user_norm = user.dropna()
-
+user_norm.to_csv('user_norm.csv', index=False)
 # 어간 추출
 # stemmed_titles = []
 # stemmer = nltk.stem.PorterStemmer()
@@ -33,16 +33,3 @@ user_norm = user.dropna()
 # book_norm['lemmatized_title'] = lemmatized_titles
 #
 # book_norm.to_csv('book_stemming_2.csv', index=False)
-
-# LabelBinarizer를 사용하여 User-ID를 벡터로 변환
-label_binarizer = LabelBinarizer()
-user_vector = label_binarizer.fit_transform(user_norm['User-ID'])
-
-# 자신의 User-ID에 해당하는 인덱스를 1로, 나머지 인덱스를 0으로 설정
-user_vector = np.where(user_vector == user_vector.max(axis=1, keepdims=True), 1, 0)
-
-# 변환된 벡터를 DataFrame에 추가
-user_vector_df = pd.DataFrame(user_vector, columns=label_binarizer.classes_)
-user_norm = pd.concat([user_norm, user_vector_df], axis=1)
-
-user_norm.to_csv('user_features.csv', index=False)
