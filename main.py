bd55428d6135eac6f148f29daf4103ff972ba092
Indwoo <wjsdlsdn1158@naver.com>
2023-12-03 20:22:34 +0900

    MSE 함수 개선3

--- main.py
+++ main.py
@@ -3,13 +3,11 @@ import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.metrics import mean_squared_error
 
-
 # 추천 행렬 함수
 def predict_rating(rating, item_sim):
     rating_pred = rating.dot(item_sim) / np.array([np.abs(item_sim).sum(axis=1)])
     rating_pred[np.isnan(rating_pred)] = 0  # 0으로 나누는 경우에 대한 예외 처리
     return rating_pred
-
 
 # MSE 함수
 def mse(pred, actual):
@@ -17,19 +15,17 @@ def mse(pred, actual):
     actual = actual[actual.nonzero()].flatten()
     return mean_squared_error(pred, actual)
 
-
 # MSE 개선 함수
 def predict_rating_top_n(rating, item_sim, n=20):
     pred = np.zeros(rating.shape)
 
     for col in range(rating.shape[1]):
-        top_n_item = np.argsort(item_sim[:, col])[:-n - 1:-1]
+        top_n_item = np.argsort(item_sim[:, col])[:-n - 1:-1]  # 스칼라로 변환
         for row in range(rating.shape[0]):
             pred[row, col] = item_sim[col, top_n_item].dot(rating[row, top_n_item].T)
             pred[row, col] /= np.sum(np.abs(item_sim[col, top_n_item]))
 
     return pred
-
 
 df = pd.read_csv('user_norm.csv')
 
@@ -43,6 +39,11 @@ df_sim = pd.DataFrame(data=sim, index=pivot_table_tr.index, columns=pivot_table_
 rating_pred = predict_rating(pivot_table.values, df_sim.values)
 rating_pred_matrix = pd.DataFrame(data=rating_pred, index=pivot_table.index, columns=pivot_table.columns)
 
-rating_pred_2 = predict_rating_top_n(pivot_table.values, df_sim.values, n=20)
+try:
+    rating_pred_2 = predict_rating_top_n(pivot_table.values, df_sim.values, n=20)
+    #print("mse: ", mse(rating_pred_2, pivot_table.values))
+    rating_pred_matrix = pd.DataFrame(data=rating_pred_2, index=pivot_table.index, columns=pivot_table.columns)
+    print(rating_pred_matrix.head())
+except KeyboardInterrupt:
+    print("오류가 발생")
 
-print("mse: ", mse(rating_pred_2, pivot_table.values))
