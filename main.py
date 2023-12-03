b958dd2b0687adfc5c443cbdbd02d538107ef247
Indwoo <wjsdlsdn1158@naver.com>
2023-12-03 17:02:28 +0900

    새로운 데이터 확인

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import pandas as pd
 import numpy as np
+import rating as rating
 
-df_user_data= pd.read_csv("./data/user_data.csv")
+df_user_data= pd.read_csv("user_rating_0_to_1000.csv")
+df_user_data= df_user_data[["ID", "Name", "Rating"]]
+
+df_user_data= rating.pivot_table("Rating", index= "ID", columns= "Name").fillna(0)
+df_user_data.head()
