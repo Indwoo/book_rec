08559c63cc53a6d1f5401035d743de743c4e2955
Indwoo <wjsdlsdn1158@naver.com>
2023-12-03 18:16:13 +0900

    행렬 함수 1

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
+import numpy as np
 import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+# 추천 행렬 함수
+def predict_rating(rating, item_sim):
+    rating_pred = rating.dot(item_sim) / np.array([np.abs(item_sim).sum(axis=1)])
+    return rating_pred
+
 
 df = pd.read_csv('user_norm.csv')
 
 pivot_table = pd.pivot_table(df, values='Rating', index='ID', columns='Name', fill_value=0)
-pivot_table_tr= pivot_table.transpose()
-print(pivot_table_tr.head())
+pivot_table_tr = pivot_table.transpose()
+
+# 유사도 행렬 생성
+sim = cosine_similarity(pivot_table_tr, pivot_table_tr)
+df_sim = pd.DataFrame(data=sim, index=pivot_table_tr.index, columns=pivot_table_tr.index)
+
+rating_pred = predict_rating(pivot_table.values, df_sim.values)
+rating_pred_matrix = pd.DataFrame(data=rating_pred, index=pivot_table.index, columns=pivot_table.columns)
+rating_pred_matrix.head()
