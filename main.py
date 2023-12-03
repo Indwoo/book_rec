327aa7a326433007461e82168b532af4802287b0
Indwoo <wjsdlsdn1158@naver.com>
2023-12-03 17:33:29 +0900

    데이터 정제

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import pandas as pd
-import numpy as np
-import rating as rating
 
-df_user_data= pd.read_csv("user_rating_0_to_1000.csv")
-df_user_data= df_user_data[["ID", "Name", "Rating"]]
+df = pd.read_csv('user_norm.csv')
 
-df_user_data= rating.pivot_table("Rating", index= "ID", columns= "Name").fillna(0)
-df_user_data.head()
+pivot_table = pd.pivot_table(df, values='Rating', index='ID', columns='Name', fill_value=0)
+
+print(pivot_table.head())
