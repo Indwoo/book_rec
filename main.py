a1415c18dfb2371b9175d68488fddbe8644f6177
Indwoo <wjsdlsdn1158@naver.com>
2023-12-03 17:52:41 +0900

    데이터 테이블 변환(transpose)

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import pandas as pd
 df = pd.read_csv('user_norm.csv')
 
 pivot_table = pd.pivot_table(df, values='Rating', index='ID', columns='Name', fill_value=0)
-
-print(pivot_table.head())
+pivot_table_tr= pivot_table.transpose()
+print(pivot_table_tr.head())
