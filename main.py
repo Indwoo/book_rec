b16c3403ff5f21767bd2e8666f6bce8368e15588
Indwoo <wjsdlsdn1158@naver.com>
2023-12-03 16:04:20 +0900

    데이터셋 전부 삭제(사용하기엔 너무 큼)

--- main.py
+++ main.py
@@ -1,61 +1,55 @@
 import pandas as pd
 import nltk
-from nltk.stem import PorterStemmer, WordNetLemmatizer
-from nltk.tokenize import word_tokenize
-import numpy as np
-from sklearn.preprocessing import LabelBinarizer, OneHotEncoder
-from random import sample
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.preprocessing import LabelBinarizer
 
+nltk.download('punkt')
+nltk.download('wordnet')
+
+# book 데이터와 user_norm 데이터를 읽어옵니다.
 book = pd.read_csv('book_stemming_2.csv')
 user_norm = pd.read_csv('cleaned_user_norm.csv')
-#rating = pd.read_csv('archive/Ratings.csv', delimiter=';')
-
-# 데이터 크기 제한을 위해 book 데이터에서 일부 데이터만 선택합니다.
-sample_size = 50000
-book_sample = book.sample(n=sample_size, random_state=42)
 
 # Title_stem 벡터 생성
-stemmed_title_counts = book_sample['stemmed_title'].apply(lambda x: nltk.word_tokenize(x)).explode().value_counts()
-stemmed_title_vector = stemmed_title_counts[stemmed_title_counts >= 1].index.tolist()
-
-title_stem_vector = []
-for title in book_sample['Title']:
-    title_tokens = nltk.word_tokenize(title)
-    title_stem = [token for token in title_tokens if token in stemmed_title_vector]
-    title_stem_vector.append(' '.join(title_stem))
+title_pipeline = Pipeline([
+    ('title_extractor', TupleExtractor(0)),
+    ('title_vectorizer', CountVectorizer(min_df=1, ngram_range=(1,3))),
+])
+title_stem_vector = title_pipeline.fit_transform(book['Title']).toarray()
 
 # Author_stem 벡터 생성
-author_stem_counts = book_sample['Author'].apply(lambda x: nltk.word_tokenize(x)).explode().value_counts()
-author_stem_vector = author_stem_counts[author_stem_counts >= 2].index.tolist()
-
-author_stem_vector = []
-for author in book_sample['Author']:
-    author_tokens = nltk.word_tokenize(author)
-    author_stem = [token for token in author_tokens if token in author_stem_vector]
-    author_stem_vector.append(' '.join(author_stem))
-
-# Publisher_stem 벡터 생성
-publisher_stem_counts = book_sample['Publisher'].apply(lambda x: nltk.word_tokenize(x)).explode().value_counts()
-publisher_stem_vector = publisher_stem_counts[publisher_stem_counts >= 2].index.tolist()
-
-publisher_stem_vector = []
-for publisher in book_sample['Publisher']:
-    publisher_tokens = nltk.word_tokenize(publisher)
-    publisher_stem = [token for token in publisher_tokens if token in publisher_stem_vector]
-    publisher_stem_vector.append(' '.join(publisher_stem))
+author_pipeline = Pipeline([
+    ('author_extractor', TupleExtractor(0)),
+    ('author_vectorizer', CountVectorizer(min_df=2, ngram_range=(1,1))),
+])
+author_stem_vector = author_pipeline.fit_transform(book['Author']).toarray()
 
 # Year_stem 벡터 생성
-label_binarizer = LabelBinarizer()
-year_stem_vector = label_binarizer.fit_transform(book_sample['Year'])
+year_pipeline = Pipeline([
+    ('year_extractor', TupleExtractor(1)),
+    ('year_vectorizer', LabelBinarizer(sparse_output=True)),
+])
+year_stem_vector = year_pipeline.fit_transform(book['Year']).toarray()
 
-# book 데이터프레임에 특징 벡터 컬럼 추가
-book_sample['Title_stem'] = title_stem_vector
-book_sample['Author_stem'] = author_stem_vector
-book_sample['Publisher_stem'] = publisher_stem_vector
-book_sample['Year_stem'] = year_stem_vector
+# Publisher_stem 벡터 생성
+publisher_pipeline = Pipeline([
+    ('publisher_extractor', TupleExtractor(2)),
+    ('publisher_vectorizer', CountVectorizer(min_df=2, ngram_range=(1,1))),
+])
+publisher_stem_vector = publisher_pipeline.fit_transform(book['Publisher']).toarray()
+
+# 생성된 특징 벡터를 데이터프레임으로 변환
+book_features = pd.DataFrame({
+    'Title_stem': title_stem_vector.tolist(),
+    'Author_stem': author_stem_vector.tolist(),
+    'Year_stem': year_stem_vector.tolist(),
+    'Publisher_stem': publisher_stem_vector.tolist()
+})
 
 # 특징 벡터가 추가된 book 데이터를 새로운 파일로 저장
-book_sample.to_csv('특징벡터_생성_제한.csv', index=False)
+book_with_features = pd.concat([book, book_features], axis=1)
+book_with_features.to_csv('book_with_features.csv', index=False)
 
 
 
