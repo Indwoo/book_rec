f8c6c925a53c04cbb81c0a29e8f3600df17035e1
Indwoo <wjsdlsdn1158@naver.com>
2023-12-03 18:28:28 +0900

    MSE 함수 생성 및 확인

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics import mean_squared_error
 
 
 # 추천 행렬 함수
@@ -8,6 +9,11 @@ def predict_rating(rating, item_sim):
     rating_pred = rating.dot(item_sim) / np.array([np.abs(item_sim).sum(axis=1)])
     return rating_pred
 
+# MSE 함수
+def mse(pred, actual):
+    pred = pred[actual.nonzero()].flatten()
+    actual = actual[actual.nonzero()].flatten()
+    return mean_squared_error(pred, actual)
 
 df = pd.read_csv('user_norm.csv')
 
@@ -20,4 +26,5 @@ df_sim = pd.DataFrame(data=sim, index=pivot_table_tr.index, columns=pivot_table_
 
 rating_pred = predict_rating(pivot_table.values, df_sim.values)
 rating_pred_matrix = pd.DataFrame(data=rating_pred, index=pivot_table.index, columns=pivot_table.columns)
-rating_pred_matrix.head()
+
+print("mse: ", mse(rating_pred, pivot_table.values))
